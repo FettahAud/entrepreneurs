0975cdaec73d20052f8bb6cddb8d75630dce28bb
fettahaud <fettahaud@gmail.com>
2024-01-21 10:13:17 +0300

    added lenis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.scss";
 import Header from "@/app/components/Header";
 import Footer from "./components/Footer";
+import SmoothScroller from "./components/Lenis";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SmoothScroller />
         <Header />
         {children}
         <Footer />
