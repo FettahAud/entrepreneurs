cb03dc4a4369c9aad45f69933870ccb18d60b3be
Abdülfettah Adwani <fettahaud@gmail.com>
2024-02-12 12:42:57 +0300

    Responsive style for 375px

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,8 +4,11 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../../../public/e-logo.svg";
 import StarBut from "./StarBut";
+import { useRef, useState } from "react";
 
 export default function Header() {
+  const mobileNav = useRef<HTMLDivElement>(null);
+  const [state, setState] = useState(true);
   const nav = [
     { name: "Accueil", href: "/", id: 0 },
     { name: "Nos Strategies", href: "/about", id: 1 },
@@ -33,21 +36,45 @@ export default function Header() {
       ],
     },
   ];
+  const handleMobileMenu = () => {
+    mobileNav.current!.classList.toggle("active");
+    setState(!state);
+    if (state) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  };
   return (
     <header id="header">
+      <svg
+        onClick={() => handleMobileMenu()}
+        className="mobile-menu-icon"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+      >
+        <path
+          d="M3 12H15M3 6H21M3 18H21"
+          stroke="#CDCDCD"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
       <Image src={logo} alt="logo" className="logo" />
       <nav>
         <ul>
           {nav.map((item) => (
             <li
               key={item.id}
-              className={item.subNav && "sub-nav-wrapper"}
-              onClick={(e) =>
-                item.subNav && e.currentTarget.classList.toggle("active")
-              }
-              onBlur={(e) =>
-                item.subNav && e.currentTarget.classList.remove("active")
-              }
+              className={item.subNav && "sub-nav-wrapper active"}
+              // onClick={(e) =>
+              //   item.subNav && e.currentTarget.classList.toggle("active")
+              // }
+              // onBlur={(e) =>
+              //   item.subNav && e.currentTarget.classList.remove("active")
+              // }
             >
               <Link href={item.href}>{item.name}</Link>
               {item.subNav && (
@@ -98,6 +125,75 @@ export default function Header() {
           ))}
         </ul>
       </nav>
+      <div ref={mobileNav} className="mobile-nav">
+        <div className="grid">
+          <img src="/Grid.png" alt="" />
+        </div>
+        <ul className="nav-list">
+          {nav.map((item) => (
+            <li key={item.id} className={item.subNav && "sub-nav-wrapper"}>
+              <Link
+                onClick={(e) =>
+                  item.subNav &&
+                  e.currentTarget.parentElement!.classList.toggle("active")
+                }
+                href={item.href}
+              >
+                {item.name}
+              </Link>
+              {item.subNav && (
+                <>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                  >
+                    <rect
+                      width="16"
+                      height="16"
+                      rx="8"
+                      fill="white"
+                      fillOpacity="0.1"
+                    />
+                    <path
+                      d="M5.5 7L8 9.5L10.5 7"
+                      stroke="white"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  <ul className="sub-nav">
+                    {item.subNav.map((item) => (
+                      <li key={item.id}>
+                        <Link href={item.href}>{item.name}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+        <div className="footer">
+          <span>Follow us</span>
+          <ul className="social-icons">
+            <li>
+              <img src="/social-icons/in.svg" />
+            </li>
+            <li>
+              <img src="/social-icons/instagram.svg" />
+            </li>
+            <li>
+              <img src="/social-icons/youtube.svg" />
+            </li>
+            <li>
+              <img src="/social-icons/tiktok.svg" />
+            </li>
+          </ul>
+        </div>
+      </div>
       <StarBut text="Réserver un Appel" />
     </header>
   );
