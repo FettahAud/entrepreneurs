292bade330e9fe9b4cd5e6f2ecac1828341bd54e
Abdülfettah Adwani <fettahaud@gmail.com>
2024-02-15 23:54:13 +0300

    fix mobile breakpoint

--- src/app/utils/calculateRem.ts
+++ src/app/utils/calculateRem.ts
@@ -1,6 +1,7 @@
 export const calculateRem = () => {
   // TODO: Fix, under 375px it's mixing px values
-  if (window.innerWidth > 1920 || window.innerWidth < 375) {
+  if (typeof window === "undefined") return;
+  if (window.innerWidth > 1920 || window.innerWidth < 420) {
     document.documentElement.style.fontSize = "16px";
     return;
   } else {
@@ -9,7 +10,7 @@ export const calculateRem = () => {
       rem = (16 / 1920) * window.innerWidth;
     } else if (window.innerWidth > 768) {
       rem = (16 / 1280) * window.innerWidth;
-    } else if (window.innerWidth > 375) {
+    } else if (window.innerWidth > 420) {
       rem = (16 / 768) * window.innerWidth;
     }
     document.documentElement.style.fontSize = `${rem}px`;
